b779679e18d621243ea4a76352358ad840291edb
duggalr <duggalr42@gmail.com>
2024-10-18 14:16:22 -0400

    index

--- app/index.py
+++ app/index.py
@@ -28,6 +28,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+fp = '/opt/elasticbeanstalk/deployment/env'
+li = open(fp).readlines()
+for line in li:
+    k, v = line.split('=')[0], line.split('=')[1]
+    os.environ[k] = v
+
+for val in os.environ:
+    print(val)
+
+print(os.environ['REDIS_URL'])
+
 # Initialize Celery
 celery = Celery(
     __name__,
