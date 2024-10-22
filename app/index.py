b836dd99a32451b58805f8e3dc1f98634bdb644d
duggalr <duggalr42@gmail.com>
2024-10-21 21:49:10 -0400

    update CORS

--- app/index.py
+++ app/index.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://www.companionai.dev"],
+    allow_origins=["http://localhost:3000", "https://staging.companionai.dev", "https://www.companionai.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
