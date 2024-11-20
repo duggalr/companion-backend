afd75c8b3daac1aba0fe33a093cc971b6762468c
duggalr <duggalr42@gmail.com>
2024-11-20 13:33:50 -0800

    refactoring deploymnet

--- app/database.py
+++ app/database.py
@@ -12,37 +12,3 @@ DATABASE_URL = f"postgresql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-# if 'LOCAL' in os.environ:
-#     from dotenv import load_dotenv, find_dotenv
-#     ENV_FILE = find_dotenv()
-#     load_dotenv(ENV_FILE)
-#     DATABASE_URL = f"postgresql://{os.environ['LOCAL_DB_USER']}:{os.environ['LOCAL_DB_USER']}@localhost/{os.environ['LOCAL_DB_NAME']}"
-
-# else:
-#     import subprocess
-#     import ast
-#     def get_environ_vars():
-#         completed_process = subprocess.run(
-#             ['/opt/elasticbeanstalk/bin/get-config', 'environment'],
-#             stdout=subprocess.PIPE,
-#             text=True,
-#             check=True
-#         )
-#         return ast.literal_eval(completed_process.stdout)
-
-#     env_vars = get_environ_vars()
-#     DATABASE_URL = f"postgresql://{env_vars['PRODUCTION_DB_USERNAME']}:{env_vars['PRODUCTION_DB_PASSWORD']}@{env_vars['PRODUCTION_DB_URL']}:5432/postgres"
-
-
-
-# from dotenv import load_dotenv, find_dotenv
-# ENV_FILE = find_dotenv()
-# load_dotenv(ENV_FILE)
-# # DATABASE_URL = f"postgresql://{os.environ['LOCAL_DB_USER']}:{os.environ['LOCAL_DB_USER']}@localhost/{os.environ['LOCAL_DB_NAME']}"
-# DATABASE_URL = os.environ['DATABASE_URL']
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
